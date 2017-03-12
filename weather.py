5bb907996da00528b5b578b9dc321d66447b16b2
mkdxdx <wildboner@gmail.com>
2017-03-12 20:20:56 +0200

    corrected code

--- weather.py
+++ weather.py
@@ -1,16 +1,13 @@
-# weather station update script
-# by mkdxdx, MIT license
-
 import pycurl,json,datetime,math,time
 from io import BytesIO
 
 # this string should be configured according to your ESP-01 config
 thermaddr = "http://station.ip:port"
-curlstring = 'http://api.openweathermap.org/data/2.5/forecast?id=YOURCITYID&units=metric&appid=YOURAPPID'
+curlstring = 'http://api.openweathermap.org/data/2.5/forecast?id=CITYID&units=metric&appid=APPID'
 
 running = True
 maxindex = 4
-maxtries = 3
+maxtries = 5
 wind_arr = [
     [22, 67, ord("N"), ord("E")],
     [68, 112, ord(" "), ord("E")],
@@ -42,59 +39,78 @@ while(running == True):
     city_name = forecast["city"]["name"]
     weather_array = []
     wlist = forecast["list"]
+
+    temp_warray = {}
+
+    ind = 0
     for fcast in wlist:
         tdat = datetime.datetime.fromtimestamp(fcast["dt"])
         day = tdat.day
-        hour = tdat.hour
-
-        if day>today and hour > 11 and hour <15:
+        hr = tdat.hour
+        if day > today:
             main = fcast["main"]
-            t_min = math.floor(main["temp_min"])
-            t_max = math.ceil(main["temp_max"])
-
-            tl_sign = ord('+')
-            th_sign = ord('+')
-
-            if (t_max<0):
-                th_sign = ord('-')
-            if (t_min<0):
-                tl_sign = ord('-')
-
-
-            hum = math.floor(main["humidity"])
-            clouds = fcast["clouds"]["all"]
-            weather_array.append([day, th_sign, abs(t_max), tl_sign, abs(t_min), hum, clouds])
-        elif (day == today and hour>=hnow and hour<=hnow+3):
+            warr = [main["temp"], fcast["clouds"]["all"], ind-1, fcast["main"]["humidity"]]
+            if day in temp_warray:
+                temp_warray[day].append(warr)
+            else:
+                ind = ind + 1
+                temp_warray[day] = []
+                temp_warray[day].append(warr)
+            #print(str(day)+" day added")
+        elif (day == today and hr>=hnow and hr<=hnow+3):
             ws = math.floor(fcast["wind"]["speed"])
             wd = math.floor(fcast["wind"]["deg"])
-            if wd<21:
+            if wd < 21:
                 wd = wd + 338
 
             wind_spd = ws
 
             for direction in wind_arr:
-                if direction[0]<=wd<=direction[1]:
+                if direction[0] <= wd <= direction[1]:
                     wind_dir[0] = direction[2];
                     wind_dir[1] = direction[3];
                     break
-            print("WIND@"+str(hour)+":"+str(wind_spd)+"ms / "+chr(wind_dir[0])+chr(wind_dir[1]))
 
 
-    index = 0
+
+    for date,day_w in temp_warray.items():
+        tmin = day_w[0][0]
+        tmax = day_w[0][0]
+        idx = day_w[1][2]
+        clo = day_w[4][1]
+        hum = day_w[0][3]
+        if idx<maxindex:
+            for hr_w in day_w:
+                tmin = min(tmin,hr_w[0])
+                tmax = max(tmax,hr_w[0])
+                hum = max(hum,hr_w[3])
+
+            tmin = math.floor(tmin)
+            tmax = math.ceil(tmax)
+
+            tl_sign = ord('+')
+            th_sign = ord('+')
+
+            if (tmax < 0):
+                th_sign = ord('-')
+            if (tmin < 0):
+                tl_sign = ord('-')
+
+            #print(str(idx)+" DAY "+""+str(date)+" / "+str(tmin)+" ~ "+str(tmax))
+            weather_array.append([idx,date,th_sign,abs(tmax),tl_sign,abs(tmin),hum,clo])
+
+
+        #weather_array.append
+
 
     postcmd = []
 
     for weather in weather_array:
-        clo = weather[6]
-        print(str(weather[0])+"\tT:"+chr(weather[1])+str(weather[2])+" ~ "+chr(weather[3])+str(weather[4])+"C\tRH:"+str(weather[5])+"%\t"+str(clo))
-        fcaststr = "FCS;"+chr(index)+chr(weather[0])+chr(weather[1])+chr(weather[2])+chr(weather[3])+chr(weather[4])+chr(weather[5])+chr(weather[6])
-        index = index + 1
+        print(str(weather[0])+": D"+str(weather[1])+" "+chr(weather[2])+str(weather[3])+" ~ "+chr(weather[4])+str(weather[5])+" C RH:"+str(weather[6])+"% V:"+str(weather[7])+"%")
+        fcaststr = "FCS;"+chr(weather[0])+chr(weather[1])+chr(weather[2])+chr(weather[3])+chr(weather[4])+chr(weather[5])+chr(weather[6])+chr(weather[7])
         postcmd.append(fcaststr)
 
-        if index >= maxindex:
-            #append wind command
-            postcmd.append("SWS;"+chr(wind_spd)+chr(wind_dir[0])+chr(wind_dir[1]))
-            break
+    postcmd.append("SWS;" + chr(wind_spd) + chr(wind_dir[0]) + chr(wind_dir[1]))
 
     for attempts in range(1,maxtries):
         for cmd in postcmd:
